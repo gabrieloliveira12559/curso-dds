06accc0cfc1df18dd36c51854968de80890c1eb1
gabrieloliveira12559 <gabrieloliveiradasilva101@gmail.com>
2025-10-22 08:02:40 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,15 @@
+matriz = [
+    ["andrew", "matheus", "yamil"],
+    ["phelipe," "rodrigo," "luan"]
+    ]
+
+print(matriz[1][0])
+
+matriz[0][0] = "gabriel"
+print("novo nome: ", matriz)
+
+matriz[0][1] = "lucca"
+print("novo nome: ", matriz)
+
+matriz[0][2] = "diego"
+print("novo nome: ", matriz)
